29b3889b41135996e650c4242dc083bb31d89894
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-01-14 07:56:27 -0500

    feat: use OrbitCOntrol and damping(amortiguamiento)

--- readme.md
+++ readme.md
@@ -1,16 +1,5 @@
 # Three.js Journey
 
-## Setup
-Download [Node.js](https://nodejs.org/en/download/).
-Run this followed commands:
+## Tareas realizadas
 
-``` bash
-# Install dependencies (only the first time)
-npm install
-
-# Run the local server at localhost:8080
-npm run dev
-
-# Build for production in the dist/ directory
-npm run build
-```
+- Hacer uso de `OrbitControls` y àplicarle `Damping` (amortiguamienti) para mover la cámara alrededor del objeto (en un control propio de la libreria Three.js)
--- src/script.js
+++ src/script.js
@@ -1,27 +1,15 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
 const canvas = document.querySelector('canvas.webgl')
+
+console.log(OrbitControls)
 
 // Sizes
 const sizes = {
   width: 800,
   height: 600,
 }
-
-/* cursor */
-const cursor = {
-  x: 0,
-  y: 0,
-}
-
-canvas.addEventListener('mousemove', (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5
-  cursor.y = -(event.clientY / sizes.height - 0.5)
-
-  // console.log(
-  //   cursor.x,
-  //   cursor.y
-  // )
-})
 
 // 1. Scene
 const scene = new THREE.Scene()
@@ -49,6 +37,15 @@ camera.position.z = 3
 
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas)
+
+// controls.target.y = 1
+// controls.update()
+
+controls.enableDamping = true
+// controls.enableZoom = false
+
 // 3. Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
@@ -61,14 +58,16 @@ const tick = () => {
   // camera.position.y = cursor.y * 4
 
   // 2do. TIPO
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
 
-  camera.position.y = cursor.y * 5
+  // camera.position.y = cursor.y * 5
 
   // 3er. TIPO
 
-  camera.lookAt(mesh.position)
+  // camera.lookAt(mesh.position)
+
+  controls.update() // no olvidar esta linea para que se aplique el damping/amotiguamiento
 
   // Render
   renderer.render(scene, camera)
